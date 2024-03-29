5b695bc68f36cea6f11320c905fa445844968067
marely <marely.carcamo@gmail.com>
2024-03-28 21:35:10 -0300

    funcion creacion article ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
-function crearTarjeta(i) {
+/**
+ * The function "crearTarjeta" generates a card element with dynamic image and text content and appends
+ * it to a specified container on the webpage.
+ * @param i - The parameter `i` in the `crearTarjeta` function is used to dynamically generate the
+ * image URL for each card based on the value of `i`. This allows the function to create different
+ * cards with different images by appending the value of `i` to the base URL for the images.
+ */
+function crearCard(i) {
 	let urlBase = "assets/img/card";
 
 	let img = urlBase + i + ".jpg";
 
-	let tarjeta = `<div class="col-sm-12 col-md-6 col-xl-3">
+	let new_card = `<div class="col-sm-12 col-md-6 col-xl-3">
 						<div class="card shadow mb-md-5 mb-sm-5" style="width: 18rem">
 							<img src="${img}" class="card-img-top" alt="..." />
 							<div class="card-body bg-viajes text-white">
@@ -17,10 +24,17 @@ function crearTarjeta(i) {
 						</div>
 					</div>`;
 
-	$("#tarjetas-destacados").append(tarjeta);
+	$("#tarjetas-destacados").append(new_card);
 }
 
-function crearIconosRRSS(i) {
+/**
+ * The function `crearIconosRRSS` creates social media icons with corresponding links and appends them
+ * to a specified container in the footer.
+ * @param i - The parameter `i` in the `crearIconosRRSS` function is used as an index to select the
+ * corresponding icon and link from the `iconos` and `enlaces` arrays. It helps in dynamically creating
+ * and adding the icon with its link to the icons box in the footer based
+ */
+function crearIconosFooter(i) {
 	// Lista de íconos de redes sociales
 	let iconos = [
 		"fa-brands fa-linkedin",
@@ -53,22 +67,67 @@ function crearIconosRRSS(i) {
 
 
 
-$(document).ready(function () {
-	// SECCION DESTACADOS - Creación de tarjetas
+/**
+ * The function `crearArticle` dynamically creates articles with icons and text, alternating their
+ * positions based on the index provided.
+ * @param i - The `crearArticle` function you provided creates an article element with an icon and some
+ * text based on the value of the parameter `i`. The `i` parameter is used to determine which icon to
+ * display in the article.
+ */
+function crearArticle(i) {
+	let iconos = [
+		"fa-solid fa-plane",
+		"fa-solid fa-mountain",
+		"fa-solid fa-route",
+	];
 
-	for (let i = 1; i <= 4; i++) {
-		crearTarjeta(i);
+	let p_iconos = $(`<p p-icons class="w-100 bg-viajes text-center">
+						<i class="${iconos[i - 1]}"></i>
+					</p>`);
+
+	let p_texto = $(
+		`<p class="p-text px-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit molestias facilis impedit officiis molestiae repellendus, aliquid amet. Voluptate, veniam dolores. At delectus nostrum aliquam ex! Ipsum, alias sequi odit et eveniet incidunt veniam dignissimos excepturi quia ullam in nesciunt accusamus ipsam dicta reiciendis voluptatibus optio quae, maxime ad vel placeat? Illo, sed amet fuga nesciunt pariatur quis perferendis, commodi voluptate eum necessitatibus ipsam sit veritatis nisi ducimus modi, praesentium nam. Incidunt temporibus fugit esse, ratione deleniti quas doloribus molestias soluta.</p> `
+	);
+
+	var primero = p_iconos;
+	var segundo = p_texto;
+	
+	if (i % 2 == 0) {
+		primero = p_texto;
+		segundo = p_iconos;
 	}
 
-	//Texto para sección Quienes Somos
-	var loremIpsum =
-		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Es fugiat ea maxime deserunt cumque recusandae facere animi delectus veniam, vero, illum rerum omnis incidunt repellendus neque eveniet quisquam earum dolorem excepturi non, nemo blanditiis nihil dolore corporis! Laboriosam esse, aut, maiores culpa corrupti iste eius laborum et est corporis sint, nihil ullam consequuntur rem maxime cupiditate expedita illo fugiat officiis aliquam exercitationem nemo. Animi rerum consectetur est corporis nobis, fuga debitis modi mollitia aperiam atque inventore doloribus quo enim, facere fugiat numquam! Vitae voluptatibus illo totam hic beatae distinctio quae dolorum modi neque, voluptatum similique rerum fugiat excepturi! Aut, minus.";
-	$(".p-text").text(loremIpsum);
+	let articulo = $(`
+		<article class="col-4">
+		</article>`);
+
+	articulo.append(primero);
+	articulo.append(segundo);
+
+	$("#article-box").append(articulo);
+}
+
+
+
+//   **************** BLOQUE PRINCIPAL  ***********************
+
+
+$(document).ready(function () {
+
+	// SECCION QUIENES SOMOS - Creación de artículos
+	for (let i = 1; i < 4; i++) {
+		crearArticle(i);
+	}
+
+	// SECCION DESTACADOS - Creación de tarjetas
+	for (let i = 1; i <= 4; i++) {
+		crearCard(i);
+	}
+
 
 	// FOOTER - ICONOS - Crear los iconos con enlaces a rrss
-
 	for (let i = 0; i <= 5; i++) {
-		crearIconosRRSS(i);
+		crearIconosFooter(i);
 	}
 
 	/* The code `$('[data-toggle="tooltip"]').tooltip();` is setting up a tooltip functionality for
